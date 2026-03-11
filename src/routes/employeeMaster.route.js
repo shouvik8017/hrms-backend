fb4952814cbea601cf86a1e746f9500bb667cde7
shouvik8017 <shouvik.webgen@gmail.com>
2026-03-11 20:03:14 +0530

    employeeMaster route update

--- src/routes/employeeMaster.route.js
+++ src/routes/employeeMaster.route.js
@@ -2,7 +2,11 @@ import { Router } from "express";
 import { createEmployeeHandler } from "../controllers/employee/master.controller.js";
 import { employeeCreationSchema } from "../validations/employee/master.validation.js";
 import validationRequest from "../middlewares/validation.middleware.js";
+import { verifyToken, authorizeRole } from "../middlewares/auth.middleware.js";
 
 export const employeeMasterRouter = Router();
+
+employeeMasterRouter.use(verifyToken);
+employeeMasterRouter.use(authorizeRole('admin'));
 
 employeeMasterRouter.post('/create', validationRequest(employeeCreationSchema), createEmployeeHandler);
